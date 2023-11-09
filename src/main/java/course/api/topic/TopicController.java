8e03a7d73ab37440870f18b7909843a137155be3
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-11-09 09:26:16 +0300

    request by topic by  id

--- src/main/java/course/api/topic/TopicController.java
+++ src/main/java/course/api/topic/TopicController.java
@@ -3,18 +3,25 @@ package course.api.topic;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TopicController {
-	@RequestMapping("/topics")
-	public List<Topic> getAllTopics() {
-		return Arrays.asList(
-            new Topic("spring", "spring Framework" , "Spring framework Description"),
-            new Topic("java", "java Framework" , "jav framework Description"),
-            new Topic("spring", "spring Framework" , "Spring framework Description")
-        );
+@Autowired
+private TopicService topicService;
+@RequestMapping("/topics")
+public List<Topic> getAllTopics() {
+		return topicService.getAllTopics();
 	}
+
+	@RequestMapping("/topics/{id}")
+	public Topic getTopic(@PathVariable String id) {
+		return topicService.getTopic(id);
+	}
+
+
 
 }
---
+++ src/main/java/course/api/topic/TopicService.java
@@ -0,0 +1,22 @@
+package course.api.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+    private List<Topic> topics = Arrays.asList(
+
+                new Topic("spring", "spring Framework" , "Spring framework Description"),
+                new Topic("java", "java Framework" , "jav framework Description"),
+                new Topic("python", "django Framework" , "django framework Description")
+        );
+    public List<Topic> getAllTopics(){
+        return topics;
+    }
+    public Topic getTopic(String id){
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+}
