9617d77f2e3bed82c8bbf8e878279cfdae72d7ce
Juma Emmanuel <jumaemmanuel462@yahoo.com>
2023-11-09 15:28:55 +0300

    Post an object of a class

--- src/main/java/course/api/topic/TopicController.java
+++ src/main/java/course/api/topic/TopicController.java
@@ -4,9 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TopicController {
@@ -21,7 +19,10 @@ public List<Topic> getAllTopics() {
 	public Topic getTopic(@PathVariable String id) {
 		return topicService.getTopic(id);
 	}
-
+	@RequestMapping(method = RequestMethod.POST, value="/topics")
+	public void addTopic(@RequestBody Topic topic) {
+		 topicService.addTopic(topic);
+	}
 
 
 }
--- src/main/java/course/api/topic/TopicService.java
+++ src/main/java/course/api/topic/TopicService.java
@@ -2,21 +2,26 @@ package course.api.topic;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @Service
 public class TopicService {
-    private List<Topic> topics = Arrays.asList(
+    private List<Topic> topics = new ArrayList<>( Arrays.asList(
 
                 new Topic("spring", "spring Framework" , "Spring framework Description"),
                 new Topic("java", "java Framework" , "jav framework Description"),
                 new Topic("python", "django Framework" , "django framework Description")
-        );
+        ));
     public List<Topic> getAllTopics(){
         return topics;
     }
     public Topic getTopic(String id){
         return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic){
+        topics.add(topic);
     }
 }
